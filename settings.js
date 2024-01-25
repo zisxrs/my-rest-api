46da610a0872b25592b5291337a5f112bf65df9f
zisxrs <76538862+zisxrs@users.noreply.github.com>
2024-01-26 00:20:41 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'Scheta'// yourname
+global.MONGO_DB_URI = "mongodb+srv://SchetaMongoDB:06052002205n1c@cluster0.j77owui.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "keguadululah" //isi apa aja bebas
+global.your_email = "vecksosz3@gmail.com" //email
+global.email_password = "fbfa stvg gpiv jthe" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
